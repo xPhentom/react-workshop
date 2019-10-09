cfe630d515d528a3dd456089f30cfa912aaeb9c7
Wouter Roozeleer <wouter.roozeleer@mediagenix.tv>
2019-10-09 09:35:24 +0200

    Code after the first workshop

---
+++ src/components/dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+import MovieCards from './movieCards'
+
+export default class Dashboard extends Component {
+    render(){
+        return(<div><MovieCards/></div>)
+    }
+}
---
+++ src/components/dashboard/movieCards/index.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import {Button} from 'react-bootstrap'
+
+export default class MovieCards extends Component {
+    render(){
+        return(<div><Button>Test</Button></div>)
+    }
+}
